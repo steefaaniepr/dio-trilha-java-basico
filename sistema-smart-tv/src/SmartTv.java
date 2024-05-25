17b8a786136d192d6b3542ea5674fcf226f75169
Stefanie Paes <steefaaniepr@outlook.com>
2024-05-24 21:42:18 -0300

    att-metodos

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,7 +1,28 @@
 public class SmartTv {
+    
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void diminuirVolume(){
+        volume--;
+    }
 
     public void ligar(){
         ligada = true;
