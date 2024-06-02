b1630ce3aa0dea6cd719dee5254740142b2a3f46
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-02 08:16:20 -0300

    att

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,17 +1,34 @@
 package candidatura; 
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
-    /**
-     * @param salarioPretendido
-     */
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0; 
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salario " + salarioPretendido );
+            if(salarioPretendido >= salarioBase) {
+                System.out.println("o candidato " + candidato+ "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    } 
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 3000);
+    }
+    
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
