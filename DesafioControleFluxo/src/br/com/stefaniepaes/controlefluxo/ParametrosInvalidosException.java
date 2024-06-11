fbc3bbc04af50d0073cd81eecb080d3b3d640fa3
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-11 01:09:59 -0300

    att exception class

--- DesafioControleFluxo/src/br/com/stefaniepaes/controlefluxo/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/br/com/stefaniepaes/controlefluxo/ParametrosInvalidosException.java
@@ -3,7 +3,7 @@ package br.com.stefaniepaes.controlefluxo;
 public class ParametrosInvalidosException {
 
     public ParametrosInvalidosException(String message) {
-        //TODO Auto-generated constructor stub
+        super();
     }
     
 }
