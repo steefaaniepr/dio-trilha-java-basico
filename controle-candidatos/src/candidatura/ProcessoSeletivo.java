b652cfd4114fb0abae9758a12d84615e55f7f130
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-10 23:56:23 -0300

    att

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -3,7 +3,23 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        //analisarCandidato();
+        //selecaoCandidatos();
+        //imprimirSelecionado();
+    }
+
+    static void imprimirSelecionado() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos  informando o indice do elemento");
+
+        for(int indice=0; indice < candidatos.length;indice++){
+            System.out.println("O candidato de n° " + (indice+1) + " é " + candidatos[indice]);
+        }
+        System.out.println("Forma abreviada de interação for ech");
+
+        for(String candidato: candidatos ){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos() {
