aed5163f865684a055027660ead3ef0f37b90c11
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-18 23:37:25 -0300

    att class ListaTarefa

--- lista-tarefa/src/ListaTarefa.java
+++ lista-tarefa/src/ListaTarefa.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaTarefa {
+    //Atributo
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefaParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefaParaRemover);
+
+    }
+    
     
 }
