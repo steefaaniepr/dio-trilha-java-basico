ff3167ee23f27849142d274d8be53fa81cf10cc4
Stefanie Paes <steefaaniepr@outlook.com>
2024-06-18 23:07:55 -0300

    att class tarefa

--- lista-tarefa/src/Tarefa.java
+++ lista-tarefa/src/Tarefa.java
@@ -1,3 +1,20 @@
 public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
     
+
+
 }
